93bd5bef674b6e04cca78b3266c6aa4522123ee4
JIAYIN <jiayinjyjy@gmail.com>
2019-11-16 18:24:28 +0800

    Updated showing

--- subway.pl
+++ subway.pl
@@ -1,14 +1,14 @@
-% Append(Merge) 2 different lists into 1
+/* Append(Merge) 2 different lists into 1 */
 append([], Y, Y).
 append([H|X], Y, [H|Z]) :- append(X, Y, Z).
 
-% Set condition of meal
+/* Set condition of meal */
 meal_healthy(healthy).
 meal_value(value).
 meal_vegan(vegan).
 meal_veggie(veggie).
 
-% Set choices 
+/* Set choices */
 meals([normal, healthy, value, vegan, veggie]).
 breads([italian_wheat, hearty_italian, honey_oat, multigrain, flatbread]).
 meats([chicken_teriyaki, cold_cut_trio, meatball_marinara_melt, roast_beef, subway_club]).
@@ -19,36 +19,43 @@ cheese_topups([processed_cheddar, monterey_cheddar]).
 non_cheese_topups([avocado, tuna]).
 sides([chips, cookie, drink]).
 
-% Show meal items one by one
+/* Show meal items one by one */
 ask_meals(X) :- meals(L), member(X,L).
 
-% Show bread items one by one
+/* Show bread items one by one */
 ask_breads(X) :- breads(L), member(X,L).
 
-% Set condition for items to be in list
-% Reduce nested list to show items one by one
+/* Set condition for items to be in list
+   Reduce nested list to show items one by one */
 ask_mt(X) :- findall(X, (meal_chosen(Y), \+meal_vegan(Y), \+meal_veggie(Y), meats(X)), X).
 ask_meat(X) :- ask_mt(L) , member(X,L).
 ask_meats(X) :- ask_meat(L) , member(X,L).
 
-% Show veggie items one by one
+/* Show veggie items one by one */
 ask_veggies(X) :- veggies(L), member(X,L).
 
-% Set condition for items to be in list
-% Reduce nested list to show items one by one
+/* Set condition for items to be in list
+   Reduce nested list to show items one by one */
 ask_ss(X) :- findall(X, (meal_chosen(Y), meal_healthy(Y) -> non_fatty_sauces(X);
                  fatty_sauces(L1), non_fatty_sauces(L2), append(L1, L2, X)), X).
 ask_sauce(X) :- ask_ss(L), member(X,L).
 ask_sauces(X) :- ask_sauce(L), member(X,L).
 
-% Set condition for items to be in list
-% Reduce nested list to show items one by one
+/* Set condition for items to be in list
+   Reduce nested list to show items one by one */
 ask_ts(X) :- findall(X, (meal_chosen(Y), \+meal_value(Y) -> (meal_vegan(Y) -> non_cheese_topups(X);
                  cheese_topups(L1), non_cheese_topups(L2), append(L1, L2, X))), X).
 ask_topup(X) :- ask_ts(L), member(X,L).
 ask_topups(X) :- ask_topup(L), member(X,L).
 
-% Show side items one by one
+/* Show side items one by one */
 ask_sides(X) :- sides(L), member(X,L).
 
-% Chosen(assert) items are saved under item_chosen 
+/* Chosen(assert) items are saved under item_chosen */
+show_meal(X) :- meal_chosen(X).
+show_bread(X) :- bread_chosen(X).
+show_meat(X) :- meat_chosen(X).
+show_veggie(X) :- veggie_chosen(X).
+show_sauce(X) :- sauce_chosen(X).
+show_topup(X) :- topup_chosen(X).
+show_side(X) :- side_chosen(X).
